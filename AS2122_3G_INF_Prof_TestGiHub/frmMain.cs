624f700450df8a8cb2e39fa1e966367a43f33908
Filippo Albertini <filippo@filippoalbertini.it>
2022-03-09 08:32:35 +0100

    Impostazione iniziale della parte di calcolo in funzione dell'operatore scelto dall'utente

--- AS2122_3G_INF_Prof_TestGiHub/frmMain.cs
+++ AS2122_3G_INF_Prof_TestGiHub/frmMain.cs
@@ -6,5 +6,20 @@ namespace AS2122_3G_INF_Prof_TestGiHub
         {
             InitializeComponent();
         }
+
+        private void btnCalcola_Click(object sender, EventArgs e)
+        {
+            double operando1 = 0, operando2 = 0, risultato = 0;
+            switch (cmbOperatore.Text)
+            {
+                case "+":
+                    break;
+                case "-":
+                    break;
+            }
+
+            // visualizzo il risultato
+            lblRisultato.Text = $"Il risultato è {risultato}";
+        }
     }
 }
